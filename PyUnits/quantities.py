7a0c31f5cf19be934a4110f3eda0be459e3fa2cb
Mattias Olla <mattias.olla@gmail.com>
2020-12-27 22:14:52 +0100

    Skip the short variable names, create some wrapper classes

--- PyUnits/quantities.py
+++ PyUnits/quantities.py
@@ -59,64 +59,38 @@ class Quantity(NamedTuple):
         return Quantity(**{symbol: val * exponent for symbol, val in self._asdict().items()})
 
 
-# region BaseQuantities
-M = Quantity(mass=1)
-L = Quantity(length=1)
-T = Quantity(time=1)
-Θ = Quantity(temperature=1)
-I = Quantity(current=1)  # noqa: E741
-N = Quantity(substance_amount=1)
-J = Quantity(luminous_intensity=1)
-A = Quantity(plane_angle=1)
-S = Quantity(solid_angle=1)
-# endregion
+class Base:
+    MASS = Quantity(mass=1)
+    LENGTH = Quantity(length=1)
+    TIME = Quantity(time=1)
+    TEMPERATURE = Quantity(temperature=1)
+    CURRENT = Quantity(current=1)
+    SUBSTANCE_AMOUNT = Quantity(substance_amount=1)
+    LUMINOUS_INTENSITY = Quantity(luminous_intensity=1)
+    PLANE_ANGLE = Quantity(plane_angle=1)
+    SOLID_ANGLE = Quantity(solid_angle=1)
 
-# region DerivedQuantities
-FREQUENCY = T ** -1
-VELOCITY = L / T
-ACCELERATON = VELOCITY / T
-FORCE = M * ACCELERATON
-MOMENTUM = M * VELOCITY
-ENERGY = FORCE * L
-AREA = L ** 2
-VOLUME = L ** 3
-ANGULAR_VELOCITY = A / T
-DENSITY = M / VOLUME
-PRESSURE = FORCE / AREA
-POWER = ENERGY / T
-CHARGE = I * T
-LUMINANCE = J / S ** 2
-HEAT_CAPACITY = ENERGY / T
-MOLARITY = M / N
-# endregion DerivedQuantities
+
+class Derived:
+    FREQUENCY = Base.TIME ** -1
+    VELOCITY = Base.LENGTH / Base.TIME
+    ACCELERATON = VELOCITY / Base.TIME
+    FORCE = Base.MASS * ACCELERATON
+    MOMENTUM = Base.MASS * VELOCITY
+    ENERGY = FORCE * Base.LENGTH
+    AREA = Base.LENGTH ** 2
+    VOLUME = Base.LENGTH ** 3
+    ANGULAR_VELOCITY = Base.PLANE_ANGLE / Base.TIME
+    DENSITY = Base.MASS / VOLUME
+    PRESSURE = FORCE / AREA
+    POWER = ENERGY / Base.TIME
+    CHARGE = Base.CURRENT * Base.TIME
+    LUMINANCE = Base.LUMINOUS_INTENSITY / Base.SOLID_ANGLE ** 2
+    HEAT_CAPACITY = ENERGY / Base.TEMPERATURE
+    MOLARITY = Base.MASS / Base.SUBSTANCE_AMOUNT
 
 
 if __name__ == "__main__":
-    for quant in [
-        M,
-        L,
-        T,
-        Θ,
-        I,
-        N,
-        J,
-        A,
-        S,
-        FREQUENCY,
-        VELOCITY,
-        ACCELERATON,
-        FORCE,
-        MOMENTUM,
-        ENERGY,
-        AREA,
-        VOLUME,
-        ANGULAR_VELOCITY,
-        DENSITY,
-        PRESSURE,
-        POWER,
-        CHARGE,
-        LUMINANCE,
-        HEAT_CAPACITY,
-        MOLARITY,
-    ]:
-        print(quant)
+    for name, rep in (vars(Base) | vars(Derived)).items():
+        if isinstance(rep, Quantity):
+            print(name, rep)
